4719c196ed83c21999396d5218046ba0515cf955
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-09-03 20:41:17 +0300

    Add swipe of image

---
+++ PhotoOfCities/City.swift
@@ -0,0 +1,26 @@
+//
+//  City.swift
+//  PhotoOfCities
+//
+//  Created by Sergey Lukaschuk on 03.09.2021.
+//
+
+import Foundation
+
+struct City {
+    var name: String
+    var image: String
+    var description: String
+    
+}
+
+extension City {
+    static var all = [
+        City.init(name: "Vienna", image: "Vienna_01", description: "Until the beginning of the 20th century, Vienna was the largest German-speaking city in the world, and before the splitting of the Austro-Hungarian Empire in World War I, the city had 2 million inhabitants. Today, it is the second-largest German-speaking city after Berlin."),
+        City.init(name: "London", image: "London_01", description: "London is the capital and largest city of England and the United Kingdom. The city stands on the River Thames in southeast England, at the head of its 50-mile (80 km) estuary leading to the North Sea. London has been a major settlement for two millennia."),
+        City(name: "New York City", image: "NewYorkCity_01", description: "New York, often called New York City to distinguish it from New York State, or NYC for short, is the most populous city in the United States. With a 2020 population of 8,804,190 distributed over 302.6 square miles (784 km2), New York City is also the most densely populated major city in the United States.")
+    ]
+}
+
+
+
--- PhotoOfCities/ViewController.swift
+++ PhotoOfCities/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var imageCity: UIImageView!
+    @IBOutlet var cityName: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    private let cities = City.all
+    private var index = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
     }
-
-
+    
+    private func updateUI() {
+        imageCity.image = UIImage(named: cities[index].image)
+        cityName.text = cities[index].name
+        descriptionLabel.text = cities[index].description
+    }
+    
+    @objc private func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipeGesture.direction {
+            case .right:
+                print("right swipe")
+                if index > 0 { index -= 1; updateUI() }
+                
+            case .left:
+                print("left swipe")
+                if index != cities.count - 1 { index += 1; updateUI() }
+                
+                
+            default:
+                break
+            }
+        }
+    }
+    
 }
 
