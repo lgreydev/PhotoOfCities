900f26ed6e51c8a9ac1b0f2d00ff4fa70bd68338
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-09-03 16:50:55 +0300

    Initial Commit

---
+++ PhotoOfCities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoOfCities
+//
+//  Created by Sergey Lukaschuk on 03.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
